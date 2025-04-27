c132d419c2735dc5953549d39ec93c953c1e14c3
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2025-04-27 21:58:59 +0530

    1- post routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import authroutes from "./routes/auth.routes.js";
 import connectDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
 import {v2 as cloudinary } from "cloudinary";
-
+import authroutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 dotenv.config();
 const app = express();
@@ -14,15 +14,17 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Routes
-app.use("/api/auth", authroutes);
-app.use("/api/user", userRoutes);
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+// Routes
+app.use("/api/auth", authroutes);
+app.use("/api/user", userRoutes);
+app.use("/api/posts", postRoutes);
 
 // Error handling middleware (optional)
 app.use((err, req, res, next) => {
